9d04a88980331bc7f44d7995eecfade0ca670a1b
Jashan-Windows <jashanbhullar376@gmail.com>
2024-12-05 11:34:23 +0530

    feat: add colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./learn/**/*.{html, js}'],
-  theme: {
-    fontFamily: {
+	content: ['./learn/**/*.{html, js}'],
+	theme: {
+		fontFamily: {
 			manrope: ['"Manrope", "sans-serif"'],
 			poppins: ['"Poppins", "sans-serif"']
 		},
-    extend: {},
-  },
-  plugins: [],
+		colors: {
+			blue: '#1A91F0',
+			'blue-mid': '#1170CD',
+			'blue-dark': '#1A1C6A',
+			white: '#FFFFFF',
+			bright: '#EFF2F9',
+			dark: '#1E2532',
+			black: '#000000',
+			grey: '#656E83',
+			green: '#084C41',
+		},
+		extend: {},
+	},
+	plugins: [],
 }
-
